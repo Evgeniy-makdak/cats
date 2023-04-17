0fa4e059f11b10717826349df32c758500da0a8f
Evgeniy <makdak23@mail.ru>
2023-04-17 21:52:43 +0300

    add main.js, modify style and index for modal window

---
+++ main.js
@@ -0,0 +1,8 @@
+const modalWindow = document.querySelector('.modal-window');
+const editButtons = document.querySelectorAll('.card__edit-button');
+
+editButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        modalWindow.showModal();
+    });
+});

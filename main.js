f4efcfe44929e1bb176ff849eed36eba0b57cd06
Evgeniy <makdak23@mail.ru>
2023-04-18 21:23:15 +0300

    minor styles and html changes

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const modalWindow = document.querySelector('.modal-window');
-const editButtons = document.querySelectorAll('.card__edit-button');
+document.addEventListener('DOMContentLoaded', () => {
+    const modalWindow = document.querySelector('.modal-window');
+    const editButtons = document.querySelectorAll('.card__edit-button');
 
-editButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        modalWindow.showModal();
+    editButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            modalWindow.showModal();
+        });
     });
 });
